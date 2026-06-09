8a61cc612f6a54f1dadaac510a86059ae8670887
Rahul Raveendran <rahulraveendran@gmail.com>
2026-06-09 13:18:49 +0530

    Fix auth middleware import

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const Product = require("../models/Product");
-const protect = require("../middleware/protect");
+const protect = require("../middleware/authMiddleware");
 const adminMiddleware = require("../middleware/adminMiddleware");
 
 /* =========================
